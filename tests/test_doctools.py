b47290e78a4104d3e9fe68633ee07999e28b9588
Kyle McCormick <kmccormick@edx.org>
2019-11-01 00:55:42 -0400

    Add dummy test

---
+++ tests/test_doctools.py
@@ -0,0 +1,12 @@
+"""
+Tests for edx-api-doc-tools.
+"""
+from __future__ import absolute_import, unicode_literals
+
+
+def test_nothing():
+    """
+    A temporary dummy test in order to test that tox is set up
+    correctly.
+    """
+    assert True
