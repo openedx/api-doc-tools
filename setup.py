fffab27b6a0d2cf6d6e36157a38b5282a7505074
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-01-25 21:00:36 +0500

    upgrading requirements.

--- setup.py
+++ setup.py
@@ -85,7 +85,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.8',
     ],
 )
